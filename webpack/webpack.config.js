3e2acf53dade8f4bb0df5d7e07c1290d4e7d1eb5
MrPauL96 <jupvillegasgo@unal.edu.co>
2019-05-06 14:56:26 -0500

    Some changes of dependencies

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,24 +6,34 @@ module.exports = {
     entry: {
         main: './src/main.ts'
     },
+
     output: {
-        path: path.join(__dirname, "../dist/"),
+        path: path.resolve(__dirname, "../dist/"),
         filename: "[name].bundle.js",
+        chunkFilename: "[name]-chunk.js",
     },
+
     resolve: {
         extensions: ['.js', '.ts', '.html']
     },
+
     devServer: {
         contentBase: path.join(__dirname, "../dist/"),
         port: 9000
     },
+
     devtool: 'inline-source-map',
+
     module: {
-        loaders: [
-            { test: /.ts$/, use: ['awesome-typescript-loader', 'angular2-template-loader'] },
-            { test: /.html$/, use: 'raw-loader' }
+        rules: [
+            { test: /\.ts$/, use: ['awesome-typescript-loader', 'angular2-template-loader'] },
+            { test: /\.(ts|js)$/, loaders: ['angular-router-loader'] }, 
+            { test: /.html$/, use: 'raw-loader' },
+            { test: /\.(s*)css$/, use: ['to-string-loader','style-loader','css-loader','sass-loader'] },
+            { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader' },
         ]
     },
+
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
@@ -33,10 +43,10 @@ module.exports = {
             path: path.join(__dirname, "../dist/"),
             hash: true
         }),
+
         new ContextReplacementPlugin(
             /angular(\\|\/)core(\\|\/)@angular/,
             path.resolve(__dirname, '../src')
         )
     ]
-    
 }
